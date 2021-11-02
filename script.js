4f470ebf37b023750beb1edc54b0228b19baf984
AripovJr <jasurbek_aripov@gmail.ru>
2021-11-02 14:20:55 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+const container = document.getElementById('container')
+const colors = ['#e74c3c', '#8e44ad', '#3498db','#e67e22','#2ecc71']
+const SQUARES = 500
+
+for(let i = 0; i<SQUARES; i++){
+    const square = document.createElement('div')
+    square.classList.add('square')
+
+    square.addEventListener('mouseover', ()=>setColors(square))
+    square.addEventListener('mouseout', ()=>removeColors(square))
+
+    container.appendChild(square)
+}
+
+function setColors(element){
+    const color = randomColor()
+    element.style.background=color
+    element.style.boxShadow=`0 0 2px ${color}, 0 0 10px${color}`
+}
+function removeColors(element){
+    element.style.background='#1d1d1d'
+    element.style.boxShadow='0 0 2px #000'
+}
+
+function randomColor(){
+    return colors[Math.floor(Math.random()*colors.length)]
+}
